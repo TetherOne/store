0640181e3c3212d250c48bf779a50ed973b69cba
Pasha Ryaguzov <pashajobber@gmail.com>
2024-07-28 23:20:29 +0500

    added gin, updated main handler

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"store"
+	"store/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(store.Server)
-	if err := srv.Run("8080", nil); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatal(err)
 	}
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,1 +1,18 @@
 package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.GET("/")
+		}
+	}
+	return router
+}
